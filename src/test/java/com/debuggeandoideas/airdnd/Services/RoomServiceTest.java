76df339237f3e275bdbd0ada1df64783d50870e5
ricardo sandoval <ricardoschmg@gmail.com>
2025-01-02 01:11:31 -0500

    nueva prueba

--- src/test/java/com/debuggeandoideas/airdnd/Services/RoomServiceTest.java
+++ src/test/java/com/debuggeandoideas/airdnd/Services/RoomServiceTest.java
@@ -1,31 +1,39 @@
 package com.debuggeandoideas.airdnd.Services;
 
 
+import com.debuggeandoideas.airdnd.Services.RoomService;
+import com.debuggeandoideas.airdnd.Services.utils.DataDummy;
 import com.debuggeandoideas.airdnd.repositories.RoomRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class RoomServiceTest {
 
-    private RoomRepository roomRepository;
+    private RoomRepository roomRepositoryMock;
     private RoomService roomService;
 
     @BeforeEach
     void init() {
-      this.roomRepository = new RoomRepository();
-      this.roomService = new RoomService(roomRepository);
+      this.roomRepositoryMock = mock(RoomRepository.class);
+      this.roomService = new RoomService(roomRepositoryMock);
     }
 
     @Test
     @DisplayName("Should get all rows available in room repository")
     void findAllAvailableRooms(){
-     var expected = 4;
+     when(roomRepositoryMock.findAll())
+             .thenReturn(DataDummy.default_rooms);
+     var expected = 3;
      var result = this.roomService.findAllAvailableRooms();
      assertEquals(expected,result.size());
-
     }
-
-
 }
--- src/test/java/com/debuggeandoideas/airdnd/Services/utils/DataDummy.java
+++ src/test/java/com/debuggeandoideas/airdnd/Services/utils/DataDummy.java
@@ -1,4 +1,24 @@
 package com.debuggeandoideas.airdnd.Services.utils;
 
+import com.debuggeandoideas.airdnd.dto.RoomDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataDummy {
+
+
+    public static final Map<RoomDto, Boolean> default_rooms = new HashMap<>() {{
+        put(new RoomDto("A", 2), true);
+        put(new RoomDto("B", 2), true);
+        put(new RoomDto("C", 3), true);
+        put(new RoomDto("D", 2), false);
+        put(new RoomDto("E", 2), false);
+        put(new RoomDto("F", 3), false);
+    }};
+
+    DataDummy (){
+
+    }
+
 }
