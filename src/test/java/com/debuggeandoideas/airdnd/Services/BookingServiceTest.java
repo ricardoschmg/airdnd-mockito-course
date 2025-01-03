37db0d05f1b3c68e3c17a755c857f06d3875d6b7
ricardo sandoval <ricardoschmg@gmail.com>
2025-01-02 20:12:20 -0500

    ajustes de pruebas

--- src/test/java/com/debuggeandoideas/airdnd/Services/BookingServiceTest.java
+++ src/test/java/com/debuggeandoideas/airdnd/Services/BookingServiceTest.java
@@ -2,27 +2,44 @@ package com.debuggeandoideas.airdnd.Services;
 
 import com.debuggeandoideas.airdnd.helpers.MailHelper;
 import com.debuggeandoideas.airdnd.repositories.BookingRepository;
+import org.junit.jupiter.api.Disabled;
+import com.debuggeandoideas.airdnd.Services.utils.DataDummy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BookingServiceTest {
 
     @Mock
     private PaymentService paymentServiceMock;
-
     @Mock
     private RoomService roomServiceMock;
-
     @Mock
     private BookingRepository bookingRepositoryMock;
-
     @Mock
     private MailHelper mailHelperMock;
-
     @InjectMocks
     private BookingService bookingService;
+
+    @Test
+    @DisplayName("prueba obtener numero de habitaciones disponibles")
+    void getAvailablePlaceCount () {
+     when (this.roomServiceMock.findAllAvailableRooms())
+             .thenReturn(DataDummy.default_rooms_list);
+
+        var expected = 14;
+        var result = this.bookingService.getAvailablePlaceCount();
+
+        assertEquals(expected,result);
+    }
+
 
 }
--- src/test/java/com/debuggeandoideas/airdnd/Services/utils/DataDummy.java
+++ src/test/java/com/debuggeandoideas/airdnd/Services/utils/DataDummy.java
@@ -3,6 +3,7 @@ package com.debuggeandoideas.airdnd.Services.utils;
 import com.debuggeandoideas.airdnd.dto.RoomDto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DataDummy {
@@ -17,8 +18,18 @@ public class DataDummy {
         put(new RoomDto("F", 3), false);
     }};
 
+    public static final List<RoomDto> default_rooms_list = List.of(
+        new RoomDto("A", 2),
+        new RoomDto("B", 2),
+        new RoomDto("C", 3),
+        new RoomDto("D", 2),
+        new RoomDto("E", 2),
+        new RoomDto("F", 3)
+    );
+
     DataDummy (){
 
     }
-
 }
+
+
